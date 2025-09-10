b811fd4222964a911d25db2db3947caf289c74a6
SarthakRawat13 <sarthak@aicraft.com.au>
2025-09-10 12:46:05 +0930

    Updated file

--- Update_System/update.sh
+++ Update_System/update.sh
@@ -5,7 +5,7 @@ read -r choice
 if [[ "$choice" == "yes" || "$choice" == "y" ]]; then
     echo "Starting system update..."
     # Update package lists and upgrade
-    if sudo apt-get update && sudo apt-get upgrade -y; then
+    if sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y libjpeg-dev zlib1g-dev libpng-dev; then
         echo "System update completed successfully."
     else
         echo "Update failed. Please try manual update."
